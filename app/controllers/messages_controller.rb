5bc93f604272c8052d8132139ca715f8f7e63246
takeshi0422 <takeshi.n0422@gmail.com>
2015-09-23 08:22:27 +0000

    textarea

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
     @messages = Message.new
   end
   
-def create
+  def create
     @message = Message.new(message_params)
     if @message.save
       redirect_to root_path , notice: 'メッセージを保存しました'
@@ -15,10 +15,27 @@ def create
       flash.now[:alert] = "メッセージの保存に失敗しました。"
       render 'index'
     end
-end
+  end
+
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
   private
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
