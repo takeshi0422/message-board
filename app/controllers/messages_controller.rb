d308d51bc24229aaa7dec1a1ce46efd5ad06a1db
takeshi0422 <takeshi.n0422@gmail.com>
2015-10-16 09:40:06 +0000

    change

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,22 +1,10 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update,:destroy]
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
   def index
     # Messageを全て取得する。
     @messages = Message.all
     @message = Message.new
-  end
-  def create
-    binding.pry
-    @message = Message.new(message_params)
-    if @message.save
-      redirect_to root_path , notice: 'メッセージを保存しました'
-    else
-      # メッセージが保存できなかった時
-      @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました。"
-      render 'index'
-    end
-<<<<<<< HEAD
   end
   
   def edit
@@ -31,18 +19,27 @@ class MessagesController < ApplicationController
       render 'edit'
     end
   end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      # メッセージが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
+  end
   
   def destroy
     @message.destroy
     redirect_to root_path, notice: 'メッセージを削除しました'
-=======
->>>>>>> challenge1
   end
 
-  
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   
   def set_message
